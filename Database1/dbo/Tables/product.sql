b5fdecfca29481f6121c147fd44e0906b963ac8c
Behruz Ruziboev <behruzbek931@gmail.com>
2021-08-28 01:41:29 +0500

    Hefladsjlkfjlasflasdkjl

--- Database1/dbo/Tables/product.sql
+++ Database1/dbo/Tables/product.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[product] (
     [product_id]       INT          NULL,
-    [product_desc]     VARCHAR (50) NULL,
+    [product_desc]     VARCHAR (230) NULL,
     [product_category] VARCHAR (50) NULL
 );
 
