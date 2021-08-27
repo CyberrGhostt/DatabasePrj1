c2704acfb3fef688394a69e6be348a736c11f147
Behruz Ruziboev <behruzbek931@gmail.com>
2021-08-28 01:40:01 +0500

    #32444 Increased SubProd column max charachter size

--- Database1/dbo/Tables/DISTINCTProducts.sql
+++ Database1/dbo/Tables/DISTINCTProducts.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[DISTINCTProducts] (
     [Id]      INT          NULL,
     [Prod]    VARCHAR (10) NULL,
-    [SubProd] VARCHAR (10) NULL,
+    [SubProd] VARCHAR (20) NULL,
     [Dt]      DATETIME     NULL
 );
 
